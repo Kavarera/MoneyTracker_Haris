78aead609603721f105916564abdef01a611ab91
Kavarera <rafli.030715@gmail.com>
2026-01-03 05:38:30 +0700

    Setup UI untuk import data

--- MoneyTracker.WinUI/Components/HandDropDownButton.cs
+++ MoneyTracker.WinUI/Components/HandDropDownButton.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.UI.Input;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace MoneyTracker.WinUI.Components
 {
-    public class HandButton : Button
+    public class HandDropDownButton : DropDownButton
     {
         protected override void OnPointerEntered(PointerRoutedEventArgs e)
         {
